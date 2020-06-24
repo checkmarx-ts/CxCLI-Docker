48d0b2900fceda9df292829587c4ace3adbcdbe2
Nathan Leach <nleach@athenahealth.com>
2020-06-24 16:53:33 -0400

    Update to fix cert import

--- import_certs.sh
+++ import_certs.sh
@@ -4,5 +4,5 @@ cd `dirname $0`
 
 for certfile in $(ls *.crt *.cer 2> /dev/null)
 do
-    keytool -importcert -file $certfile -alias $certfile -keystore $JAVA_HOME/lib/security/cacerts -storepass changeit -noprompt
+    keytool -importcert -file $certfile -alias $certfile -keystore $JAVA_HOME/jre/lib/security/cacerts -storepass changeit -noprompt
 done
